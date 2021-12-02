e216aa8751fafb3acddadf9f7115c99fa97ae9bd
CarlosDLopez <94584840+CarlosDLopez@users.noreply.github.com>
2021-12-01 22:22:20 -0800

    Add files via upload

--- AERO_351_Project.m
+++ AERO_351_Project.m
@@ -43,7 +43,7 @@ Pegasus.Period = COES(7);
 state = [PegRvect PegVvect];
 tspan = [0 Pegasus.tsp];
 options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
-[time,PegasusRV] = ode45(@EOM, tspan, state,options);
+[~,PegasusRV] = ode45(@EOM, tspan, state,options);
 
 hold on 
 plot3(PegasusRV(:,1), PegasusRV(:,2), PegasusRV(:,3));
@@ -83,7 +83,7 @@ tLamb = 87.6*60; %Seconds
 
 tspan = [0 Falcon.tsp+FalconDifference+tLamb];
 
-[time,FalconRV] = ode45(@EOM, tspan, state,options);
+[~,FalconRV] = ode45(@EOM, tspan, state,options);
 
 hold on 
 plot3(FalconRV(:,1), FalconRV(:,2), FalconRV(:,3));
@@ -115,8 +115,8 @@ COES = COEs(AyaRvect, AyaVvect);
 Ayame.Period = COES(7);
 %:) COEs from the ECI r and v vector match with TLE COE data
 state = [AyaRvect AyaVvect];
-tspan = [0 Ayame.tsp+AyameDifference];
-[time,AyameRV] = ode45(@EOM, tspan, state,options);
+tspan = [0 Ayame.tsp+AyameDifference+(5*Falcon.Period)];
+[~,AyameRV] = ode45(@EOM, tspan, state,options);
 
 hold on 
 plot3(AyameRV(:,1), AyameRV(:,2), AyameRV(:,3));
@@ -146,7 +146,7 @@ Titan.Period = COES(7);
 state = [TitRvect TitVvect];
 tspan = [0 Titan.tsp+TitanDifference];
 
-[time,TitanRV] = ode45(@EOM, tspan, state,options);
+[~,TitanRV] = ode45(@EOM, tspan, state,options);
 
 hold on 
 plot3(TitanRV(:,1), TitanRV(:,2), TitanRV(:,3));
@@ -199,16 +199,13 @@ legend("Pegasus R/B Orbit", "Pegasus R/B ", "Falcon 1 R/B Orbit", "Falcon 1 R/B"
 Faltr2t = Falcon.tsp+FalconDifference+tLamb+(5*Falcon.Period);
 
 tspan = [0 Faltr2t];
-state = [FalRvect FalVvect]
-[Ftime,FalconRV] = ode45(@EOM, tspan, state,options);
+state = [FalRvect FalVvect];
+[~,FalconRV] = ode45(@EOM, tspan, state,options);
 
+figure(3)
 plot3(FalconRV(:,1), FalconRV(:,2), FalconRV(:,3),'b');
 hold on
 plot3(FalconRV(end,1), FalconRV(end,2), FalconRV(end,3),'b.', 'MarkerSize',10)
-
-Ayatr2t = Ayame.tsp+AyameDifference+tLamb+(5*Falcon.Period);
-tspan = [0 Ayatr2t];
-[~,AyameRV] = ode45(@EOM, tspan, state,options);
 
 %Circularize orbit of Falcon
 timeleft2perigee = (7*Falcon.Period)-Faltr2t;
@@ -223,12 +220,18 @@ hold on
 plot3(FalconRV(end,1), FalconRV(end,2), FalconRV(end,3),'c.', 'MarkerSize',10)
 hold on 
 plot3(FalRvect(1),FalRvect(2),FalRvect(3),'r.', 'MarkerSize', 10)
+grid on 
+grid minor
+xlabel('x')
+ylabel('y')
+zlabel('z')
+title("Waiting till Falcon hits it's perigee point")
 
 rpfalcon=norm(FalconRV(end,1:3));
 
 hnew = sqrt(rpfalcon*muearth);
 
-%Falcon 1 Rocket Body TLE Data
+%Falcon 1 with Circular orbit
 CircFalcon.h = hnew;
 CircFalcon.inc = 9.0452*(pi/180);
 CircFalcon.RAAN = 264.0368*(pi/180);
@@ -237,15 +240,118 @@ CircFalcon.w = 331.4079*(pi/180);
 
 [CircFalRvect, CircFalVvect] = PerigeeRandV(CircFalcon.h, CircFalcon.ecc, CircFalcon.RAAN, CircFalcon.inc, CircFalcon.w);
 
-deltaVcircl = norm(CircFalVvect-FalVvect);
 
-%Inclination change
-deltainc = Ayame.inc- Falcon.inc;
+%Delta V to circularize Falcon at its perigee
+vcirc = sqrt(muearth/rpfalcon);
+vpFalcon = norm(FalconRV(end,4:6));
 
-v1 = norm([FalconRV(end,4) FalconRV(end,5) FalconRV(end,6)]);
+deltaVcirc = sqrt((vpFalcon^2)+(vcirc^2)-(2*vpFalcon)*(vcirc)*cos(0));
 
-deltVinc = 2*v1*sin(deltainc/2);
+%disp("The Delta V to circularize Falcon is " + double(deltaVcirc) + " km/s")
 
-%RAAN Change
+%Inc and RAAN Change together
+v1 = norm(CircFalVvect);
 deltaRAAN = Ayame.RAAN-Falcon.RAAN;
 
+alpha = acos((cos(Falcon.inc)*cos(Ayame.inc)) + (sin(Falcon.inc)*sin(Ayame.inc)*cos(deltaRAAN)));
+deltaVIncRAAN = norm(2*v1*sin(alpha/2));
+
+%disp("The DeltaV for an inc and RAAN change is " + double(deltaVIncRAAN) + " km/s")
+
+%Falcon 1 with Circular orbit and Inc and RAAN change
+CIRFalcon.h = hnew;
+CIRFalcon.inc = Ayame.inc;
+CIRFalcon.RAAN = Ayame.RAAN;
+CIRFalcon.ecc =  0;
+CIRFalcon.w = 331.4079*(pi/180);
+
+[CIRFalRvect, CIRFalVvect] = PerigeeRandV(CIRFalcon.h, CIRFalcon.ecc, CIRFalcon.RAAN, CIRFalcon.inc, CIRFalcon.w);
+
+COES = COEs(CIRFalRvect, CIRFalVvect);
+CIRFalcon.Period = COES(7);
+
+tspan = [0 CIRFalcon.Period];
+state = [CIRFalRvect CIRFalVvect];
+[~,CIRFalconRV] = ode45(@EOM, tspan, state,options);
+fuckingwork = [CIRFalconRV(end,1), CIRFalconRV(end,2), CIRFalconRV(end,3)];
+
+figure(4)
+plot3(CIRFalconRV(:,1), CIRFalconRV(:,2), CIRFalconRV(:,3), 'g');
+hold on
+plot3(CIRFalconRV(end,1), CIRFalconRV(end,2), CIRFalconRV(end,3),'.',"Color", [0.5 .6 .2], 'MarkerSize',10)
+
+% CEOs of new big circ orbit
+BigCirc.inc = Ayame.inc;
+BigCirc.RAAN = Ayame.RAAN;
+BigCirc.w = 331.4079*(pi/180);
+BigCirc.ecc = 0; 
+BigCirc.h = sqrt(norm(AyaRvect)*muearth);
+
+[BigCircRvect, BigCircVvect] = PerigeeRandV(BigCirc.h, BigCirc.ecc, BigCirc.RAAN, BigCirc.inc, BigCirc.w);
+
+COES = COEs(BigCircRvect,BigCircVvect);
+BigCirc.Period = COES(7);
+
+state = [BigCircRvect BigCircVvect];
+tspan = [0 (BigCirc.Period/2)-184.8153];
+    
+[~,BigCircRV] = ode45(@EOM, tspan, state, options);
+
+hold on 
+plot3(BigCircRV(:,1), BigCircRV(:,2), BigCircRV(:,3));
+hold on 
+plot3(BigCircRV(end,1), BigCircRV(end,2), BigCircRV(end,3), 'g.', 'MarkerSize', 10);
+title('Increasing size of Obrit to intersect with Ayame Orbit')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+%Delta V to increase orbit size
+vBigCirc = BigCirc.h/norm(AyaRvect);
+deltaVBigCirc = abs(BigCirc.h/norm(AyaRvect) - CIRFalcon.h/norm(CIRFalRvect));
+
+%disp("The DeltaV to expand the circular orbit is " + double(deltaVBigCirc) + " km/s")
+
+intialvelocity = BigCircRV(end,4:6);
+finalvelocity = AyaVvect;
+
+deltaVdirectionchange = norm(finalvelocity-intialvelocity);
+
+timesincestart = 7*Falcon.Period+.001+(BigCirc.Period/2)-184.8153;
+tspan = [0 timesincestart];
+state = [AyaRvect AyaVvect];
+[~,AyameRV] = ode45(@EOM, tspan, state,options);
+
+hold on 
+plot3(AyameRV(:,1), AyameRV(:,2), AyameRV(:,3));
+hold on
+plot3(AyameRV(end,1), AyameRV(end,2), AyameRV(end,3),'c.', 'MarkerSize',10)
+grid on 
+grid minor
+
+%Finding out how many periods Ayame has finished
+ayatl2p = timesincestart-Ayame.Period;
+%This also equals the distance from where we are to where the Ayame debris is 
+
+%Want to do a inner chase maneuever because we are behind the obejct
+Ttransfer = Ayame.Period-ayatl2p;
+trperigee = norm(AyaRvect);
+%Solving for the semi major axis of the transfer orbit
+ta = ((Ttransfer*sqrt(muearth))/(2*pi))^(2/3);
+%Solving for the radius of apogee of the transfer orbit
+trapogee = (2*ta)-trperigee;
+%Solving for the eccentricity of the new orbit
+tecc = (trapogee-trperigee)/(trapogee+trperigee);
+%Solving for the angular momentum of the transfer orbit
+th = sqrt(trperigee*muearth*(1+tecc));
+
+transfer.h = th;
+transfer.ecc = tecc;
+transfer.RAAN = Ayame.RAAN;
+transfer.w = Ayame.w;
+transfer.inc = Ayame.inc;
+
+[transferRvect, transferVvect] = PerigeeRandV(transfer.h, transfer.ecc, transfer.RAAN, transfer.inc, transfer.w);
+
+deltaVchase = norm(transferVvect-AyaVvect)*2;
+disp("Total deltaV required for transfer 2 is " + double(deltaVdirectionchange+deltaVBigCirc+deltaVIncRAAN+deltaVcirc+deltaVchase) + ' km/s')
